07f4dbb53ddc14f7fcaf94e32795b30d1d82298a
Kebbre <a.pescher@bondbryan.co.uk>
2025-12-18 14:20:59 +0000

    02.01.01

--- modules/constants.js
+++ modules/constants.js
@@ -1,16 +1,16 @@
 const TASK_COLOR_MAP = {
-  'Bond Bryan Design': 'rgba(220, 230, 247, 1)',
-  'Bond Bryan review': 'rgba(181, 182, 205, 1)',
-  'CDP': 'rgba(218, 235, 226, 1)',
-  'Client Comments': 'rgba(242, 225, 206, 1)',
-  'Delays': 'rgba(215, 215, 215, 1)',
-  'Undefined': 'rgba(249, 249, 249, 1)'
+  'Bond Bryan Design': '#00ccde',
+  'Bond Bryan review': '#ff8fad',
+  'CDP': '#ff8200',
+  'Client Comments': '#fff229',
+  'Delays': '#5cba45',
+  'Undefined': '#d1c9ba'
 };
 
-const DEFAULT_SEGMENT_COLOR = '#d0cac3ff';
-const STAGE_COLOR = 'rgba(250, 250, 210, 1)';
-const SINGLE_EVENT_COLOR = '#2fb879';
-const SINGLE_EVENT_SELECTED_COLOR = '#228a5f';
+const DEFAULT_SEGMENT_COLOR = '#00ccde';
+const STAGE_COLOR = '#d1c9ba';
+const SINGLE_EVENT_COLOR = '#5cba45';
+const SINGLE_EVENT_SELECTED_COLOR = '#ff8fad';
 
 const colorConstants = Object.freeze({
   TASK_COLOR_MAP: Object.freeze({ ...TASK_COLOR_MAP }),
